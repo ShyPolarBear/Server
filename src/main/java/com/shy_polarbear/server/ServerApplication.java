38c5752e995d4159d0e6ad6b308ce85de1057e85
0sunset0 <chi6465618@naver.com>
2023-06-08 20:08:37 +0900

    initial commit

---
+++ src/main/java/com/shy_polarbear/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.shy_polarbear.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shy_polarbear/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shy_polarbear.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
