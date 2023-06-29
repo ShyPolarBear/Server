3e6b22c82a0e2a8cd426ac770d25fbaa1d197d2a
0sunset0 <chi6465618@naver.com>
2023-06-29 21:46:23 +0900

    feat: Feed 도메인 엔티티 정의

---
+++ src/main/java/com/shy_polarbear/server/domain/feed/entity/Like.java
@@ -0,0 +1,30 @@
+package com.shy_polarbear.server.domain.feed.entity;
+
+import com.shy_polarbear.server.domain.comment.entity.Comment;
+import com.shy_polarbear.server.domain.user.entity.User;
+import com.shy_polarbear.server.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
