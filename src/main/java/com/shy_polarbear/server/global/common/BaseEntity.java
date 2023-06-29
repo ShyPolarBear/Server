c45a1c9d63672d0e8e47e655b052144919a0a2f9
0sunset0 <chi6465618@naver.com>
2023-06-29 21:50:00 +0900

    feat: 공통 엔티티 정의

---
+++ src/main/java/com/shy_polarbear/server/global/common/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.shy_polarbear.server.global.common;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    protected String createdAt;
+
+    @LastModifiedDate
+    protected String modifiedAt;
+}
