6606ac0b22c0e9dce8ca7a78a87cd311f542a7c1
0sunset0 <chi6465618@naver.com>
2023-07-03 16:57:51 +0900

    feat: 마지막 로그인 시점 필드 추가

--- src/main/java/com/shy_polarbear/server/domain/user/entity/DormantUser.java
+++ src/main/java/com/shy_polarbear/server/domain/user/entity/DormantUser.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -26,5 +27,6 @@ public class DormantUser extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
     private boolean isBlackListUser;
+    private LocalDateTime lastLoginDate;
 
 }
