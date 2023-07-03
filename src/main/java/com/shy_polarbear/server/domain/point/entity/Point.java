430dedcf32da4097ce5630d0a110ec25252855e0
0sunset0 <chi6465618@naver.com>
2023-07-03 17:00:04 +0900

    feat: point 엔티티 추가

--- src/main/java/com/shy_polarbear/server/domain/point/entity/Point.java
+++ src/main/java/com/shy_polarbear/server/domain/point/entity/Point.java
@@ -1,23 +1,47 @@
 package com.shy_polarbear.server.domain.point.entity;
 
+import com.shy_polarbear.server.domain.ranking.entity.Ranking;
+import com.shy_polarbear.server.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Getter
-public enum Point {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Point {
 
-    CREATE_FEED(500), //환경피드 작성
-    LIKE_RECEIVED(10), //내가 쓴 환경 피드에 좋아요를 받은 경우
-    BEST_FEED(2000), //내가 쓴 환경 피드가 베스트 피드가 된 경우 (베스트 피드 == 좋아요 50개 이상)
-    SOLVE_QUIZ(100), //환경 문제를 맞춘 경우
-    DELETE_FEED(-500), //환경 피드 삭제
-    LIKE_RECEIVED_CANCEL(-10), //내가 쓴 환경 피드에 좋아요가 취소된 경우
-    DELETE_BEST_FEED(2000), //베스트 피드를 삭제한 경우 (베스트 피드 == 좋아요 50개 이상)
-    REPORT_RECEIVED(500); //각종 신고를 당한 경우
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id")
+    private Long id;
 
-    private int value;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    Point(int value) {
-        this.value = value;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ranking_id")
+    private Ranking ranking;
+
+    @Enumerated(EnumType.STRING)
+    private PointType pointType;
+
+    @Builder
+    private Point(User user, PointType pointType) {
+        this.user = user;
+        this.pointType = pointType;
     }
 
+    public static Point createPoint(User user, PointType pointType) {
+        Point point = Point.builder()
+                .user(user)
+                .pointType(pointType)
+                .build();
+        user.addPoint(point);
+        return point;
+    }
 }
---
+++ src/main/java/com/shy_polarbear/server/domain/point/entity/PointType.java
@@ -0,0 +1,23 @@
+package com.shy_polarbear.server.domain.point.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum PointType {
+
+    CREATE_FEED(500), //환경피드 작성
+    LIKE_RECEIVED(10), //내가 쓴 환경 피드에 좋아요를 받은 경우
+    BEST_FEED(2000), //내가 쓴 환경 피드가 베스트 피드가 된 경우 (베스트 피드 == 좋아요 50개 이상)
+    SOLVE_QUIZ(100), //환경 문제를 맞춘 경우
+    DELETE_FEED(-500), //환경 피드 삭제
+    LIKE_RECEIVED_CANCEL(-10), //내가 쓴 환경 피드에 좋아요가 취소된 경우
+    DELETE_BEST_FEED(2000), //베스트 피드를 삭제한 경우 (베스트 피드 == 좋아요 50개 이상)
+    REPORT_RECEIVED(500); //각종 신고를 당한 경우
+
+    private int value;
+
+    PointType(int value) {
+        this.value = value;
+    }
+
+}
