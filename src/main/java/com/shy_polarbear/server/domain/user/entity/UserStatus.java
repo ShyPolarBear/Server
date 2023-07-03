f56d740bbb315ebc49070961a911931bb2247191
0sunset0 <chi6465618@naver.com>
2023-07-03 19:22:02 +0900

    feat: 중복 신고 불가를 위해 REPORTED 추가

--- src/main/java/com/shy_polarbear/server/domain/user/entity/UserStatus.java
+++ src/main/java/com/shy_polarbear/server/domain/user/entity/UserStatus.java
@@ -1,6 +1,6 @@
 package com.shy_polarbear.server.domain.user.entity;
 
 public enum UserStatus {
-    // 활성, 휴면, 탈퇴, 정지
-    ACTIVE, DORMANT, WITHDRAWN, SUSPENDED
+    // 활성, 휴면, 탈퇴, 정지, 신고당함
+    ACTIVE, DORMANT, INACTIVE, SUSPENDED, REPORTED
 }
