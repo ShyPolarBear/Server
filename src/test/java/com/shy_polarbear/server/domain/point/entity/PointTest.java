db524b859a59eb53093ca21ec02b1492b4e99938
0sunset0 <chi6465618@naver.com>
2023-07-04 17:36:11 +0900

    test: 도메인 테스트

---
+++ src/test/java/com/shy_polarbear/server/domain/point/entity/PointTest.java
@@ -0,0 +1,36 @@
+package com.shy_polarbear.server.domain.point.entity;
+
+import com.shy_polarbear.server.domain.user.entity.User;
+import com.shy_polarbear.server.domain.user.entity.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("Point 클래스")
+class PointTest {
+
+    private User user;
+    private final String nickName = "노을";
+    private final String email = "chi6465618@naver.com";
+    private final String profileImage = "";
+    private final String phoneNumber = "01093926465";
+    private final UserRole userRole = UserRole.USR;
+
+    @BeforeEach
+    void setUp() {
+        user = User.createUser(nickName, email, profileImage, phoneNumber, userRole);
+    }
+
+    @DisplayName("Point 객체가 생성되면 User의 point 값이 변경된다.")
+    @Test
+    void createPoint() {
+        Point.createPoint(user, PointType.CREATE_FEED);
+        Point.createPoint(user, PointType.BEST_FEED);
+
+        int sum = user.getPoints().stream()
+                .mapToInt(point -> point.getPointType().getValue()).sum();
+        assertThat(sum).isEqualTo(PointType.CREATE_FEED.getValue() + PointType.BEST_FEED.getValue());
+    }
+}
---
+++ src/test/java/com/shy_polarbear/server/domain/quiz/entity/UserQuizTest.java
@@ -0,0 +1,16 @@
+package com.shy_polarbear.server.domain.quiz.entity;
+
+import com.shy_polarbear.server.domain.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+
+@DisplayName("UserQuiz 클래스")
+class UserQuizTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+}
