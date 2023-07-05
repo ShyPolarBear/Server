0b98c32f3a44ddabe6d6af0a47424a655bdff001
0sunset0 <chi6465618@naver.com>
2023-07-05 20:22:55 +0900

    refactor: BaseEntity 상속

--- src/main/java/com/shy_polarbear/server/domain/user/entity/WithdrawnUser.java
+++ src/main/java/com/shy_polarbear/server/domain/user/entity/WithdrawnUser.java
@@ -1,5 +1,6 @@
 package com.shy_polarbear.server.domain.user.entity;
 
+import com.shy_polarbear.server.global.common.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +11,11 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class WithdrawnUser {
+public class WithdrawnUser extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "withdrawn_user_id")
     private Long id;
-    private LocalDateTime withdrawalTime;
     private Long userId;
 }
