0aa329bde18161f05389e5f6597d3a548bfff0e2
0sunset0 <chi6465618@naver.com>
2023-07-11 15:28:10 +0900

    fix: mysql 예약어인 값 변경

--- src/main/java/com/shy_polarbear/server/domain/user/model/UserStatus.java
+++ src/main/java/com/shy_polarbear/server/domain/user/model/UserStatus.java
@@ -2,5 +2,5 @@ package com.shy_polarbear.server.domain.user.model;
 
 public enum UserStatus {
     // 활성, 휴면, 탈퇴, 정지, 신고당함
-    ACTIVE, DORMANT, INACTIVE, SUSPENDED, REPORTED
+    ENGAGED, DORMANT, WITHDRAWN, SUSPENDED, REPORTED
 }
