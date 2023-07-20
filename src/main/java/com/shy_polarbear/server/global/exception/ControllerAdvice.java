6b12a4b5ef6c39b3a5794548a3691ce192f7a6bc
0sunset0 <chi6465618@naver.com>
2023-07-20 21:02:48 +0900

    !HOTFIX: 카카오 토큰 에러 처리

--- src/main/java/com/shy_polarbear/server/global/exception/ControllerAdvice.java
+++ src/main/java/com/shy_polarbear/server/global/exception/ControllerAdvice.java
@@ -11,10 +11,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerAdvice {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ApiResponse> exceptionHandler(CustomException ex) {
+    public ResponseEntity<ApiResponse> customExceptionHandler(CustomException ex) {
         int httpCode = ex.exceptionStatus.getHttpCode();
         int code = ex.exceptionStatus.getCustomErrorCode();
         String message = ex.exceptionStatus.getMessage();
+
+        log.warn("{}({}) - {}", ex.getClass().getSimpleName(), code, message);
+        return ResponseEntity.status(httpCode)
+                .body(ApiResponse.error(code, message));
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException ex) {
+        int httpCode = ExceptionStatus.SERVER_ERROR.getHttpCode();
+        int code = ExceptionStatus.SERVER_ERROR.getCustomErrorCode();
+        String message = ExceptionStatus.SERVER_ERROR.getMessage();
 
         log.warn("{}({}) - {}", ex.getClass().getSimpleName(), code, message);
         return ResponseEntity.status(httpCode)
