73ca1972e2e022ed16a81f1ff43c706f2b7c9aa0
wonseok2877 <wonseok2877@gmail.com>
2023-08-17 21:11:16 +0900

    feat: 비즈니스 로직 상수 추가

---
+++ src/main/java/com/shy_polarbear/server/global/common/constants/BusinessLogicConstants.java
@@ -0,0 +1,9 @@
+package com.shy_polarbear.server.global.common.constants;
+
+public class BusinessLogicConstants {
+
+    /** 퀴즈 **/
+    public static final int POINT_RESOLVED_QUIZ = 100;
+    public static final int OX_QUIZ_TIME_LIMIT = 17;
+    public static final int MULTIPLE_CHOICE_QUIZ_TIME_LIMIT = 17;
+}
