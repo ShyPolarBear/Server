7015734eb7899ec0fdb31adc39975782f4454c25
wonseok2877 <wonseok2877@gmail.com>
2023-08-18 20:11:49 +0900

    feat: 포인트 환경문제 로직 추가

---
+++ src/main/java/com/shy_polarbear/server/domain/point/service/PointService.java
@@ -0,0 +1,31 @@
+package com.shy_polarbear.server.domain.point.service;
+
+import com.shy_polarbear.server.domain.point.model.Point;
+import com.shy_polarbear.server.domain.point.model.PointType;
+import com.shy_polarbear.server.domain.point.repository.PointRepository;
+import com.shy_polarbear.server.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PointService {
+    private final PointRepository pointRepository;
+
+    // 환경 문제 맞춘 경우
+    @Transactional(propagation = Propagation.MANDATORY)  // TODO: 트랜잭션 전파 고민. 포폴 먹거리
+    public int calculateQuizSubmissionPoint(boolean isCorrect, User user) {
+        if (!isCorrect) {// 정답 여부에 따라 다르게 계산
+            return PointType.NOT_SOLVE_QUIZ.getValue();
+        } else {
+            pointRepository.save(Point.createPoint(user, PointType.SOLVE_QUIZ));
+            return PointType.SOLVE_QUIZ.getValue();
+        }
+    }
+}
