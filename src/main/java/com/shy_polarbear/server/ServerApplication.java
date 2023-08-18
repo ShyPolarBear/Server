2704e9d554eb5aadf1ed990ebcc26468fcd2eb2c
wonseok2877 <wonseok2877@gmail.com>
2023-08-19 07:17:43 +0900

    fix: @EnableJpaAuditing 추가

--- src/main/java/com/shy_polarbear/server/ServerApplication.java
+++ src/main/java/com/shy_polarbear/server/ServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
