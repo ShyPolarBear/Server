085151ce4c06f269540b8b434d52478bfe429992
wonseok2877 <wonseok2877@gmail.com>
2023-08-20 15:38:42 +0900

    refactor: 데일리 풀이 여부 조회 로직 변

--- src/main/java/com/shy_polarbear/server/domain/quiz/service/QuizService.java
+++ src/main/java/com/shy_polarbear/server/domain/quiz/service/QuizService.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,7 +34,7 @@ public class QuizService {
     private final MultipleChoiceQuizRepository multipleChoiceQuizRepository;
     private final MultipleChoiceRepository multipleChoiceRepository;
 
-    //     데일리 퀴즈 조회 : 현재 유저가 아직 풀지 않은 OX 퀴즈, 객관식 퀴즈. 최신순으로 정렬 -> 1개만
+    // 데일리 퀴즈 조회 : 오늘 유저가 아직 제출하지 않은 OX 퀴즈, 객관식 퀴즈. 최신순으로 정렬 -> 1개만
     public QuizCardResponse getDailyQuiz(Long currentUserId) {
         Quiz quiz = quizRepository.findRecentQuizNotYetSolvedByUser(currentUserId)
                 .orElseThrow(() -> new QuizException(ExceptionStatus.NO_MORE_DAILY_QUIZ));
@@ -53,18 +52,19 @@ public class QuizService {
         return PageResponse.of(result, count);
     }
 
-    // 데일리 퀴즈 풀이 여부 조회 : 오늘 0시 0분 0초를 기준으로 해당 유저가 정답을 맞춘 문제를 조회
+    // 데일리 퀴즈 풀이 여부 조회 : 오늘 0시 0분 0초를 기준으로 해당 유저가 제출한 문제를 조회
     public WhetherDailyQuizSolvedResponse getWhetherDailyQuizSolved(Long currentUserId) {
         LocalDate today = LocalDate.now();
-        Optional<UserQuiz> optionalUserQuiz = userQuizRepository.findFirstByCreatedDateStartingWithAndUserIdAndCorrectTrue(today.toString(), currentUserId);
+        Optional<UserQuiz> optionalUserQuiz = userQuizRepository.findFirstByCreatedDateStartingWithAndUserIdOrderByCreatedDateDesc(today.toString(), currentUserId);
 
-        boolean isSolved = optionalUserQuiz.isPresent();    // 레코드 존재 여부
-        Long quizId = isSolved ? optionalUserQuiz.get().getQuiz().getId() : null; // 존재 여부에 따라 id값 할당
+        boolean isSubmitted = optionalUserQuiz.isPresent();    // 레코드 존재 여부
+        Long quizId = isSubmitted ? optionalUserQuiz.get().getQuiz().getId() : null; // 존재 여부에 따라 id값 할당
+        boolean isSolved = isSubmitted && optionalUserQuiz.get().isCorrect();
 
         return WhetherDailyQuizSolvedResponse.of(quizId, isSolved);
     }
 
-    // OX 퀴즈 채점
+    // OX 퀴즈 제출 채점
     @Transactional
     public OXQuizScoreResponse scoreOXQuizSubmission(Long currentUserId, long quizId, OXQuizScoreRequest request) {
         User user = userService.getUser(currentUserId);
@@ -85,7 +85,7 @@ public class QuizService {
         return OXQuizScoreResponse.of(oxQuiz, isCorrect, pointValue);
     }
 
-    // 객관식 퀴즈 채점
+    // 객관식 퀴즈 제출 채점
     @Transactional
     public MultipleChoiceQuizScoreResponse scoreMultipleQuizSubmission(Long currentUserId, long quizId, MultipleChoiceQuizScoreRequest request) {
         User user = userService.getUser(currentUserId);
