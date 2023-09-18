42146b2085819b3ec067c4c90fd7c98501d68c37
wonseok2877 <wonseok2877@gmail.com>
2023-09-18 17:32:03 +0900

    fix: Feed 더미 데이터 imageUrls

--- src/test/java/com/shy_polarbear/server/domain/feed/template/FeedTemplate.java
+++ src/test/java/com/shy_polarbear/server/domain/feed/template/FeedTemplate.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class FeedTemplate {
     public static Feed createDummyFeed() {
         User dummyUser = UserTemplate.createDummyUser();
-        List<String> imageUrls = List.of("image.com");
+        List<String> imageUrls = List.of("https://example.com/image.jpg");
         return Feed.createFeed("피드 제목", "피드 내용", FeedImage.createFeedImages(imageUrls), dummyUser);
     }
 }
