22aa72639445663e6d95e100a2ca54fd5b858aff
wonseok2877 <wonseok2877@gmail.com>
2024-01-20 13:04:16 +0900

    refactor: Quiz 엔티티 QuizType 필드 추가

--- src/main/java/com/shy_polarbear/server/domain/quiz/model/MultipleChoiceQuiz.java
+++ src/main/java/com/shy_polarbear/server/domain/quiz/model/MultipleChoiceQuiz.java
@@ -1,17 +1,17 @@
 package com.shy_polarbear.server.domain.quiz.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Entity
-@DiscriminatorValue("M")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MultipleChoiceQuiz extends Quiz {
     @OneToMany(mappedBy = "multipleChoiceQuiz", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -19,6 +19,6 @@ public class MultipleChoiceQuiz extends Quiz {
 
     @Builder
     public MultipleChoiceQuiz(String question, String explanation) {
-        super(question, explanation);
+        super(QuizType.MULTIPLE_CHOICE, question, explanation);
     }
 }
--- src/main/java/com/shy_polarbear/server/domain/quiz/model/OXQuiz.java
+++ src/main/java/com/shy_polarbear/server/domain/quiz/model/OXQuiz.java
@@ -1,19 +1,15 @@
 package com.shy_polarbear.server.domain.quiz.model;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
-// TODO: Getter, constructor, builder은 어디서 만드는게 좋지? 자식쪽에서 하는게 맞을듯. Quiz에서 만들고 다 적용되면 좋긴한데.
 @Getter
 @Entity
-@DiscriminatorValue("OX")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OXQuiz extends Quiz {
     @Enumerated(EnumType.STRING)
@@ -21,7 +17,7 @@ public class OXQuiz extends Quiz {
 
     @Builder
     public OXQuiz(String question, String explanation, OXChoice answer) {
-        super(question, explanation);
+        super(QuizType.OX, question, explanation);
         this.answer = answer;
     }
 }
